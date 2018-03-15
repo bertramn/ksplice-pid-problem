9f39e55be9aa0d845f7162c742e0501bbd06d423
Niels Bertram <nielsbne@gmail.com>
2018-03-15 16:51:10 +1000

    added MBean pool name dump

--- src/main/java/com/github/bertramn/PID.java
+++ src/main/java/com/github/bertramn/PID.java
@@ -3,11 +3,31 @@ package com.github.bertramn;
 import jnr.posix.POSIX;
 import jnr.posix.POSIXFactory;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryPoolMXBean;
+import java.util.List;
+
 public class PID {
 
     public static void main(String[] args) {
         POSIX posix = POSIXFactory.getPOSIX();
-        System.out.println(posix.getpid());
+        System.out.println(String.format("Process ID (must not be 0): %d\n", posix.getpid()));
+
+        System.out.println("Available MBean Memory Pools:");
+        List<MemoryPoolMXBean> pools = ManagementFactory.getMemoryPoolMXBeans();
+        boolean psOldGenFound = false;
+        for (MemoryPoolMXBean pool : pools) {
+            String poolName = pool.getName();
+            if ("PS Old Gen".equals(poolName)) {
+                psOldGenFound = true;
+            }
+            System.out.println(String.format("  %s", poolName));
+        }
+
+        if (!psOldGenFound) {
+            System.out.println("\nPS Old Gen pool mbean is missing.");
+        }
+
     }
 
 }
